9115ddc6fdbda887b950aaf12a64d573607f1f4d
tashfeensaeed <tashfeen.saeed@chicagobooth.edu>
2020-09-15 19:32:40 +0000

    Plots saved

--- task02.r
+++ task02.r
@@ -2,5 +2,8 @@ earnings <- read.csv("https://raw.githubusercontent.com/avehtari/ROS-Examples/ma
 
 fitted_model <- lm(earnings$earn ~ earnings$height)
 #write.table(fitted_model, file = “my_data.txt”, sep = “”)
+png(filename="plot.png")
 plot(earnings$height, earnings$earn)
+dev.off()
+
 print(summary(fitted_model))
